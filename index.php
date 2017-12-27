dc1cbe73e4fff7ea28c7b1dd40bca505d5a4089f
Bonber Rash <bonber@protonmail.com>
2017-12-27 23:24:49 +0100

    .

--- index.php
+++ index.php
@@ -3,9 +3,25 @@
 	$sql = "SELECT * FROM productos";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
+		echo '<rss version="2.0">
+				<channel>
+				    <title>ZDE</title>
+				    <link>https://txapitel.herokuapp.com/</link>
+				    <language>es-ES</language>
+				    <description>Productos</description>
+				    <generator>Autor del RSS</generator>';
 	    while($row = $result->fetch_assoc()) {
 	        echo "id: " . $row["id"]. " - Nombre: " . $row["nombre"]. " - Precio" . $row["precio"]. " - Descripcion" . $row["descripcion"]. "<br>";
+	        echo '<item>
+			    	<id>'.$row["id"].'</id>
+					<title>'.$row["nombre"].'</title>
+					<precio>'.$row["precio"].'</precio>
+					<description><![CDATA['.$row["descripcion"].']]></description>
+					<content:encoded><![CDATA[]]></content:encoded>
+				  </item>'; 
 	    }
+	    echo '</channel>
+				  </rss>';
 	} else {
 	    echo "0 results";
 	}
