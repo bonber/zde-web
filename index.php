621454f2b2bd8cf511a29e075b4408af24aaa0f3
Bonber Rash <bonber@protonmail.com>
2017-12-27 23:09:08 +0100

    .

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
 	include("conexion.php");
-	echo "Prueba";
+	$sql = "SELECT id, firstname, lastname FROM MyGuests";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+	    while($row = $result->fetch_assoc()) {
+	        echo "id: " . $row["id"]. " - Nombre: " . $row["nombre"]. " - Precio" . $row["precio"]. " - Descripcion" . $row["descripcion"]. "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}
 ?>
